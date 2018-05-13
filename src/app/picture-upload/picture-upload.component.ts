a34be93a500cdc5e630afe2541b5664462d93b06
Daniel Oram <danieloramnz@gmail.com>
2018-05-13 15:58:39 +1200

    html/ts for upload component

--- src/app/picture-upload/picture-upload.component.ts
+++ src/app/picture-upload/picture-upload.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import Cosmic from 'cosmicjs';
+import { CosmicConfigService } from '../cosmic-config.service';
 @Component({
-  selector: 'app-picture-upload',
-  templateUrl: './picture-upload.component.html',
-  styleUrls: ['./picture-upload.component.css']
+    selector: 'picture-upload',
+    templateUrl: './picture_upload.html'
 })
-export class PictureUploadComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PictureUploadComponent {
+    private fl;
+    private title;
+    public uploading;
+    @Output() onUpload = new EventEmitter<any>();
+    constructor(
+        private cosmicConfig: CosmicConfigService
+    ) {
+        this.uploading = false;
+        this.fl = null;
+        this.title = "";
+    }
+    onFileChange(ev) {
+        if (ev.target.files && ev.target.files.length) {
+            this.fl = ev.target.files[0];
+        }
+    }
+    upload() {
+        this.uploading = true;
+        Cosmic.addMedia(this.cosmicConfig.getWriteCfg(), {
+            media: this.fl,
+            folder: this.fl.name
+        }, (error, response) => {
+            Cosmic.addObject(this.cosmicConfig.getWriteCfg(),
+                this.cosmicConfig.buildPhotoUploadObj(this.title, response.body.media.name),
+            (error, response) => {
+                this.title = '';
+                this.fl = null;
+                this.uploading = false;
+                this.onUpload.emit({});
+            });
+        });
+    }
 }
