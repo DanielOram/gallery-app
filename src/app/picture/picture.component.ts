27fd5f71fedb8953d3b6541c046fc466afad3425
Daniel Oram <danieloramnz@gmail.com>
2018-05-13 15:51:33 +1200

    html/ts for picture component

--- src/app/picture/picture.component.ts
+++ src/app/picture/picture.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-picture',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./picture.component.css']
 })
 export class PictureComponent implements OnInit {
+
+  @Input() picture: any;
 
   constructor() { }
 
