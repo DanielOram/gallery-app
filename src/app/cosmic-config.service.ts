30767eba517b8250430124843dc8389ed3d31715
Daniel Oram <danieloramnz@gmail.com>
2018-05-13 15:49:31 +1200

    create config service for cosmicjs

--- src/app/cosmic-config.service.ts
+++ src/app/cosmic-config.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import { environment } from '../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+
+@Injectable()
 export class CosmicConfigService {
-
-  constructor() { }
+    private write_key;
+    private bucket_name;
+    private photos_type;
+    constructor() {
+        this.photos_type = environment.photos_type;
+        this.write_key = environment.write_key;
+        this.bucket_name = environment.bucket_name
+    }
+    public getReadCfg(): any {
+        return {
+            bucket: {
+                slug: this.bucket_name
+            }
+        };
+    }
+    public getWriteCfg(): any {
+        return {
+            bucket: {
+                slug: this.bucket_name,
+                write_key: this.write_key
+            }
+        };
+    }
+    public buildPhotoUploadObj(title, file): any {
+        return {
+            write_key: this.write_key,
+            type_slug: this.photos_type,
+            title: title,
+            metafields: [{
+                key: 'picture',
+                type: 'file',
+                value: file
+            }]
+        };
+    }
+    getPhotoSlug() {
+        return this.photos_type;
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  write_key: 'YOURWRITEKEY',
+  bucket_name: 'YOURBUCKETNAME',
+  photos_type: 'photos'
 };
 
 /*
