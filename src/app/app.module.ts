292f5e1d5aacf1b040132aad3eb7403a911bc2a7
Daniel Oram <danieloramnz@gmail.com>
2018-05-13 15:51:59 +1200

    create picture-upload component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PictureComponent } from './picture/picture.component';
+import { PictureUploadComponent } from './picture-upload/picture-upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PictureComponent
+    PictureComponent,
+    PictureUploadComponent
   ],
   imports: [
     BrowserModule
